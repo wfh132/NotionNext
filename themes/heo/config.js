135dc30beb4af90614455b965aca930acee75c3e
wfh132 <104284186+wfh132@users.noreply.github.com>
2024-10-13 01:51:32 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
   HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
-
+  
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
